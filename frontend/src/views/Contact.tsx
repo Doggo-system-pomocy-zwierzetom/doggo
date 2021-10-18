13a62b7c034f8fdc5519a7333ef8fafdc42e0dc2
Mila <e_zamorowska@o2.pl>
2021-10-18 15:17:18 +0200

    aa

--- frontend/src/views/Contact.tsx
+++ frontend/src/views/Contact.tsx
@@ -1,7 +1,17 @@
 function Contact(){
 
     return(
-        <p>Contact</p>
+        <div>
+        <p>Napisz do nas</p>
+        <form>
+            <label>Imię:</label>
+            <input type="text" id="fname" name="fname" required/>
+            <label>E-mail:</label>
+            <input type="email" id="fmail" name="fmail" required/>
+            <label>Wiadomość:</label>
+            <input type="text" id="fcontent" name="fcontent" required/>
+        </form>
+        </div>
     )
 }
 export default Contact;
