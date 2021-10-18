7fb7cf288d3d824e5b43e3454e0d7b32f34c9a61
Piotr T <ptyburczy@gmail.com>
2021-10-18 13:45:20 +0200

    Dodano style do Navbara

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-
-function Navbar(){
-
-    return(
-        <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-        </div>
-    )
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+const StyledNavbar = styled.div`
+  background: gray;
+  display: flex;
+`;
+function Navbar() {
+  return (
+    <StyledNavbar>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+    </StyledNavbar>
+  );
 }
 export default Navbar;
