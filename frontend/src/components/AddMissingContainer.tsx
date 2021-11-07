6aefff3f51b4b8fc6dff3ed006f4437e49732f25
Mila <e_zamorowska@o2.pl>
2021-11-07 12:01:01 +0100

    Model klasyfikujący

--- frontend/src/components/AddMissingContainer.tsx
+++ frontend/src/components/AddMissingContainer.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import '@tensorflow/tfjs-backend-webgl';
+import {classes} from '../classes';
+import { useState } from 'react';
+
 const StyledAddMissingContainer = styled.div`
   padding: 2rem;
   background: white;
@@ -10,14 +14,33 @@ const StyledAddMissingContainer = styled.div`
     margin: 0;
   }
 `;
+
 function AddMissingContainer({ setIsAddMissingClicked }: any) {
+
+  const [confirmedImg, confirmImg] = useState(true);
+
+  const handleUpload = async (e:any) => {
+    const mobilenet = require('@tensorflow-models/mobilenet');
+    var img = e.target.files[0];
+    var image:any = document.createElement('img');
+    image.src = URL.createObjectURL(img);
+  
+    const model = await mobilenet.load();
+    const prediction = await model.classify(image);
+  
+    const found = prediction.some((item: any) => classes.includes(item.className))
+    confirmImg(found);
+  }
+
   return (
     <StyledAddMissingContainer>
       <h1>Zgłaszanie zaginięcia</h1>
       <label htmlFor="imie">Imie</label>
-      <input id="imie" type="text" />
+      <input id="imie" type="text" required/>
+      <input id="missingImg" type="file" onChange={(e) => handleUpload(e)} required/>
+      {!confirmedImg ? (<p>Nieprawidłowe zdjęcie, upewnij się, że znajduje się na nim odpowiednie zwierzę</p>) : ''}
       <div>
-        <button type="submit">Wyślij zgłoszenie</button>
+        <button type="submit" disabled={!confirmedImg}>Wyślij zgłoszenie</button>
         <button onClick={() => setIsAddMissingClicked(false)}>Anuluj</button>
       </div>
     </StyledAddMissingContainer>
