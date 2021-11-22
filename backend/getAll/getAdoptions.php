b8ba549c0cb13547dcf83fcc4612dbbee02d7647
Piotr T <ptyburczy@gmail.com>
2021-11-22 13:25:35 +0100

    3307

--- backend/getAll/getAdoptions.php
+++ backend/getAll/getAdoptions.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type:application/json;charset=utf-8');
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli("localhost", "root", "", "test");
+$mysqli = new mysqli("localhost:3307", "root", "", "test");
 
 $mysqli->select_db("test");
 if ($result = $mysqli -> query("SELECT * FROM adoptions")) {
