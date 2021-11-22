295d421a1799b79dd62ed48b8dedbfabd72068f1
Piotr T <ptyburczy@gmail.com>
2021-11-22 14:14:00 +0100

    adoption fetch

--- frontend/src/views/AdoptionView.tsx
+++ frontend/src/views/AdoptionView.tsx
@@ -1,8 +1,35 @@
 import AdoptionContainer from '../components/AdoptionContainer';
 import styled from 'styled-components';
+
+import { useEffect, useState } from 'react';
+
 const StyledAdoptionView = styled.div``;
 
 export default function AdoptionView() {
+  const [data, setData] = useState(Array);
+
+  async function getData() {
+    // fetch('/api/faults/list', { credentials: 'include' })
+    fetch('https://localhost:3307/getAll/getUsers.php', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <StyledAdoptionView>
       <h1>Adoptuj</h1>
@@ -10,3 +37,5 @@ export default function AdoptionView() {
     </StyledAdoptionView>
   );
 }
+
+// "proxy": "http://localhost:3307/getAll",
