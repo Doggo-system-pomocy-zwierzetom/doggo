5cb65ec700f5677b9c9bb2f7b9b0aeb1a1709813
Piotr T <ptyburczy@gmail.com>
2021-12-16 01:12:13 +0100

    PORT nie port

--- index.js
+++ index.js
@@ -26,10 +26,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 console.log(`🟢 ${process.env.NODE_ENV}🟢`);
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(process.env.port || 5000, () => console.log(`🚀🚀🚀 Server running on port: ${PORT} 🚀🚀🚀`))
-  )
+  .then(() => app.listen(PORT, () => console.log(`🚀🚀🚀 Server running on port: ${PORT} 🚀🚀🚀`)))
   .catch((error) => console.log(error.message));
