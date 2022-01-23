243485169f8f9c1dadf7a0eea281b71ae88fd607
Piotr T <ptyburczy@gmail.com>
2022-01-23 16:18:15 +0100

    Widok rejestracji

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,21 +1,80 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 // import { MyContext } from '../contexts/MyContext';
 import styled from 'styled-components';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 import { signin, signup } from '../actions/auth';
 import { useDispatch } from 'react-redux';
 import { useHistory, Link, Redirect } from 'react-router-dom';
 import { LoginInfoContext } from '../contexts/LoginInfoContextProvider';
 import axios from 'axios';
 import { emitKeypressEvents } from 'readline';
+import dog from '../img/pngwing.com.png';
 
 const StyledRegisterView = styled.main`
   /* display: flex; */
   /* gap: 1rem; */
-  max-width: 500px;
+  max-width: 650px;
+  display: flex;
+  align-items: center;
+  /* padding-left: 2rem; */
   /*form {
     display: flex;
   }*/
+  .card {
+    margin-top: 3.5rem;
+    margin-left: auto;
+    .card-body {
+      h1 {
+        text-align: center;
+      }
+      padding: 1.5rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      input[type='checkbox'] {
+        height: 1em;
+        width: 1em;
+        margin-right: 0.3rem;
+
+        &:checked {
+          background: var(--second);
+        }
+      }
+    }
+    form {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      .register-button {
+        margin-top: 1rem;
+        background: var(--second);
+        padding: 1rem;
+        font-size: 1.2em;
+        font-weight: 600;
+        border: none;
+      }
+    }
+    .link {
+      text-align: center;
+      a {
+        color: var(--main);
+      }
+    }
+  }
+  .error-message {
+    color: var(--warning);
+    text-align: center;
+    margin: 0;
+  }
+  .dog-image {
+    max-height: 400px;
+    /* margin-top: 15rem; */
+    margin-right: 0;
+    margin-left: -3px;
+    /* position: relative; */
+    /* z-index: 1; */
+  }
 `;
 
 export default function RegisterView() {
@@ -68,51 +127,155 @@ export default function RegisterView() {
       })
       .catch(function (error) {
         setErrorMessage('Coś poszło nie tak...');
-       // console.log(error);
+        // console.log(error);
       });
   };
-  const handleUserChange = (e: any) => {setFormUserData({ ...formUserData, [e.target.name]: e.target.value })};
-  const handleShelterChange = (e: any) => {setFormShelterData({ ...formShelterData, [e.target.name]: e.target.value });};
+  const handleUserChange = (e: any) => {
+    setFormUserData({ ...formUserData, [e.target.name]: e.target.value });
+  };
+  const handleShelterChange = (e: any) => {
+    setFormShelterData({ ...formShelterData, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    ifShelter ? setFormShelterData(initialShelterState) : setFormUserData(initialUserState);
+  }, [ifShelter]);
   return (
-    <>{user ? history.push('/') : 
-        
+    <>
+      {user ? (
+        history.push('/')
+      ) : (
         <StyledRegisterView>
-          
-          <h1>Rejestracja{ifShelter ? ' schroniska' : ' użytkownika'}</h1>
-          <button onClick={()=>{setIfShelter(false); setFormUserData(initialUserState);}}>Użytkownik</button><button onClick={()=>{setIfShelter(true); setFormShelterData(initialShelterState)}}>Schronisko</button>
-          <p style={{color: "red"}}>{errorMessage}</p>
-          {ifShelter ?
-          (<Form onSubmit={handleSubmitShelter}>
-            <Form.Control value={formShelterData.name} name="name" placeholder="Nazwa" onChange={handleShelterChange} />
-            <Form.Control value={formShelterData.NIP} name="NIP" placeholder="NIP" onChange={handleShelterChange}/>
+          <Card>
+            <Card.Body>
+              {/* <h1>Rejestracja{ifShelter ? ' schroniska' : ' użytkownika'}</h1> */}
+              <h1>Rejestracja</h1>
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
+              {/* <p className="registration-type"> */}
+              <div>
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  checked={ifShelter}
+                  onChange={() => setIfShelter(!ifShelter)}
+                  id="check"
+                />
+                <label htmlFor="check">Chcę utworzyć konto jako schronisko.</label>
+              </div>
 
-            <Form.Control value={formShelterData.email} name="email" placeholder="E-mail" type="email" onChange={handleShelterChange}/>
-            <Form.Control value={formShelterData.password} name="password" placeholder="Hasło" type="password" onChange={handleShelterChange}/>
+              {/* </p> */}
+              {/* <button
+                onClick={() => {
+                  setIfShelter(false);
+                  setFormUserData(initialUserState);
+                }}
+              >
+                Użytkownik
+              </button>
+              <button
+                onClick={() => {
+                  setIfShelter(true);
+                  setFormShelterData(initialShelterState);
+                }}
+              >
+                Schronisko
+              </button> */}
+              {ifShelter ? (
+                <Form onSubmit={handleSubmitShelter}>
+                  <Form.Control
+                    value={formShelterData.name}
+                    name="name"
+                    placeholder="Nazwa"
+                    onChange={handleShelterChange}
+                  />
+                  <Form.Control
+                    value={formShelterData.NIP}
+                    name="NIP"
+                    placeholder="NIP"
+                    onChange={handleShelterChange}
+                  />
 
-            <Form.Control value={formShelterData.confirmPassword} name="confirmPassword" placeholder="Potwierdź hasło" type="password" onChange={handleShelterChange}/>
-            <Button type="submit">Zarejestruj się</Button>
-            <p className="link">
-              Posiadasz już konto?
-              <Link to="/logowanie">Zaloguj się</Link>
-            </p>
-          </Form>)
-          :
-          (<Form onSubmit={handleSubmitUser}>
-            <Form.Control value={formUserData.firstName} name="firstName" placeholder="Imię" onChange={handleUserChange} />
-            <Form.Control value={formUserData.lastName} name="lastName" placeholder="Nazwisko" onChange={handleUserChange}/>
+                  <Form.Control
+                    value={formShelterData.email}
+                    name="email"
+                    placeholder="E-mail"
+                    type="email"
+                    onChange={handleShelterChange}
+                  />
+                  <Form.Control
+                    value={formShelterData.password}
+                    name="password"
+                    placeholder="Hasło"
+                    type="password"
+                    onChange={handleShelterChange}
+                  />
 
-            <Form.Control value={formUserData.email} name="email" placeholder="E-mail" type="email" onChange={handleUserChange}/>
-            <Form.Control value={formUserData.password} name="password" placeholder="Hasło" type="password" onChange={handleUserChange}/>
+                  <Form.Control
+                    value={formShelterData.confirmPassword}
+                    name="confirmPassword"
+                    placeholder="Potwierdź hasło"
+                    type="password"
+                    onChange={handleShelterChange}
+                  />
+                  <Button className="register-button" type="submit">
+                    Zarejestruj się
+                  </Button>
+                  {/* <p className="link">
+                    Posiadasz już konto?
+                    <Link to="/logowanie">Zaloguj się</Link>
+                  </p> */}
+                </Form>
+              ) : (
+                <Form onSubmit={handleSubmitUser}>
+                  <Form.Control
+                    value={formUserData.firstName}
+                    name="firstName"
+                    placeholder="Imię"
+                    onChange={handleUserChange}
+                  />
+                  <Form.Control
+                    value={formUserData.lastName}
+                    name="lastName"
+                    placeholder="Nazwisko"
+                    onChange={handleUserChange}
+                  />
 
-            <Form.Control value={formUserData.confirmPassword} name="confirmPassword" placeholder="Potwierdź hasło" type="password" onChange={handleUserChange}/>
-            <Button type="submit">Zarejestruj się</Button>
-            <p className="link">
-              Posiadasz już konto?
-              <Link to="/logowanie">Zaloguj się</Link>
-            </p>
-          </Form>)}
+                  <Form.Control
+                    value={formUserData.email}
+                    name="email"
+                    placeholder="E-mail"
+                    type="email"
+                    onChange={handleUserChange}
+                  />
+                  <Form.Control
+                    value={formUserData.password}
+                    name="password"
+                    placeholder="Hasło"
+                    type="password"
+                    onChange={handleUserChange}
+                  />
+
+                  <Form.Control
+                    value={formUserData.confirmPassword}
+                    name="confirmPassword"
+                    placeholder="Potwierdź hasło"
+                    type="password"
+                    onChange={handleUserChange}
+                  />
+                  <Button className="register-button" type="submit">
+                    Zarejestruj się
+                  </Button>
+                </Form>
+              )}
+              <p className="link">
+                {'Posiadasz już konto? '}
+                <Link to="/logowanie">Zaloguj się</Link>
+              </p>
+            </Card.Body>
+          </Card>
+          <img className="dog-image" src={dog} alt="" />
         </StyledRegisterView>
-      }
+      )}
     </>
   );
 }
