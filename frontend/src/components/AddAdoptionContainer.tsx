7047a65ac10929c517a3bc1035360f95e5626fc1
Piotr T <ptyburczy@gmail.com>
2022-01-25 19:20:37 +0100

    Wysyłanie zdjęć adopcje - test

--- frontend/src/components/AddAdoptionContainer.tsx
+++ frontend/src/components/AddAdoptionContainer.tsx
@@ -5,19 +5,18 @@ import { Card, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 const StyledAddAdoptionContainer = styled.div`
-  
-background: var(--white);
-width: 800px;
-margin: auto;
-margin-top: 2rem;
-padding-top: 2rem;
-padding-bottom: 2rem;
-box-shadow: 0 0 20px -5px var(--outline-darken);
+  background: var(--white);
+  width: 800px;
+  margin: auto;
+  margin-top: 2rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  box-shadow: 0 0 20px -5px var(--outline-darken);
   border: 0.1rem solid var(--outline);
   border-radius: 8px;
-.card{
+  .card {
     max-width: 700px;
-  margin: auto;
+    margin: auto;
   }
   .btn-delete {
     margin-top: 2rem;
@@ -25,10 +24,11 @@ box-shadow: 0 0 20px -5px var(--outline-darken);
   .btn-more {
     margin-top: 2rem;
   }
-  
 `;
 
 function AddAdoptionContainer({ setIsAddMissingClicked }: any) {
+  const [imagePreview, setImagePreview] = useState<any>('');
+  const [file, setFile] = useState<string>('');
   // const [confirmedImg, confirmImg] = useState(true);
 
   // const handleUpload = async (e: any) => {
@@ -72,6 +72,61 @@ function AddAdoptionContainer({ setIsAddMissingClicked }: any) {
     setIsAddMissingClicked(false);
   }
 
+  const photoUpload = (e: any) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file2 = e.target.files[0];
+    // console.log('reader', reader);
+
+    if (reader !== undefined && file2 !== undefined) {
+      reader.onloadend = () => {
+        console.log(file);
+
+        setFile(file2);
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file2);
+    }
+  };
+
+  async function saveImageToServer(file: any) {
+    console.log(file);
+    await fetch('https://api.imgur.com/3/image', {
+      //mode: 'cors',
+      method: 'POST',
+      headers: {
+        Authorization: 'Client-ID e938cb3d41df2b6',
+      },
+      body: file,
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          // console.log(response);
+          return response.json();
+        }
+      })
+      .then((json) => {
+        // console.log(json);
+        console.log(json.data.link);
+        setAdoption({ ...adoption, image: json.data.link });
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const onFileSubmit = (e: any) => {
+    e.preventDefault();
+    if (file !== '') saveImageToServer(file);
+    else console.log('Nie wybrano pliku!');
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, [adoption.image]);
+
   return (
     <StyledAddAdoptionContainer>
       <Card className="card">
@@ -81,7 +136,7 @@ function AddAdoptionContainer({ setIsAddMissingClicked }: any) {
         <Card.Body>
           <Card.Text></Card.Text>
 
-          <Form onSubmit={() => handleSubmit()}>
+          <Form onSubmit={(e) => onFileSubmit(e)}>
             <Form.Label htmlFor="name">Imie</Form.Label>
             <Form.Control
               id="name"
@@ -93,20 +148,35 @@ function AddAdoptionContainer({ setIsAddMissingClicked }: any) {
             <Form.Control
               id="description"
               type="text"
-              as="textarea" rows={3}
+              as="textarea"
+              rows={3}
               onChange={(e) => setAdoption({ ...adoption, description: e.target.value })}
               required
             />
-            <Form.Label htmlFor="image">Link do zdjęcia</Form.Label>
+            <Form.Label htmlFor="image">Zdjęcie</Form.Label>
+            {imagePreview !== '' && (
+              <img className="image-preview" src={imagePreview} alt="Podgląd zdjęcia" />
+            )}
+
             <Form.Control
+              type="file"
+              name="avatar"
+              id="file"
+              accept=".jpef, .png, .jpg"
+              onChange={photoUpload}
+              src={imagePreview}
+            />
+            {/* <Form.Control
               id="image"
               type="text"
               onChange={(e) => setAdoption({ ...adoption, image: e.target.value })}
               required
-            />
+            /> */}
             <Form.Group>
-              <Button className="btn-more" type="submit">Wyślij zgłoszenie</Button>
-              <Button  className="btn-delete" onClick={() => setIsAddMissingClicked(false)}>
+              <Button className="btn-more" type="submit">
+                Wyślij zgłoszenie
+              </Button>
+              <Button className="btn-delete" onClick={() => setIsAddMissingClicked(false)}>
                 Anuluj
               </Button>
             </Form.Group>
